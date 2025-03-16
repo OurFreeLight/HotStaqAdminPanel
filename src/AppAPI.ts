5627a4b2c06190380f8439608b9360e7daf91b49
ncoonrod <ncoonrod@freelight.org>
2025-03-16 01:34:43 -0700

    fix: corrected testing and dev env setup;

--- src/AppAPI.ts
+++ src/AppAPI.ts
@@ -1,16 +1,43 @@
-import { HotAPI, HotServer, HotClient, HotRoute, 
-	HotRouteMethod, MySQLSchema, 
-	ServerAuthorizationFunction, HotStaq, HotServerType, DeveloperMode, HotDBMySQL } from "hotstaq";
-import { DataRoute } from "@hotstaq/dataroute";
+import { HotAPI, HotServer, HotClient, MySQLSchema, HotServerType, HotDBMySQL, HotLog, ServerRequest} from "hotstaq";
+import { AdminRoute, User, UserRoute } from "@hotstaq/userroute";
+
+/**
+ * An admin route that can be used for any user.
+ */
+export class FriendlyAdminRoute extends AdminRoute
+{
+	constructor (api: HotAPI, routeName: string = "admins")
+	{
+		super (api, routeName);
+	}
+
+	/**
+	 * This disables the need to check for authentication.
+	 */
+	protected async checkAuth (req: ServerRequest): Promise<void>
+	{
+	}
+}
 
 /**
  * The App's API and routes.
  */
 export class AppAPI extends HotAPI
 {
-	constructor (baseUrl: string, connection: HotServer | HotClient, db: any = null)
+	/**
+	 * The logger.
+	 */
+	logger: HotLog;
+	/**
+	 * The database connection.
+	 */
+	db: HotDBMySQL;
+
+	constructor (baseUrl: string, connection: HotServer | HotClient, db: HotDBMySQL = null)
 	{
 		super(baseUrl, connection, db);
+
+		this.logger = connection.logger;
 
 		this.onPreRegister = async (): Promise<boolean> =>
 			{
@@ -23,64 +50,34 @@ export class AppAPI extends HotAPI
 			};
 		this.onPostRegister = async (): Promise<boolean> =>
 			{
-				// Sync database tables here.
+				await User.syncTables (this.db, true);
+
+				const maxUsers: number = 53;
+
+				for (let iIdx = 0; iIdx < maxUsers; iIdx++)
+				{
+					try
+					{
+						let testUser = new User ({
+								displayName: `John${iIdx}`,
+								email: `test${iIdx}@freelight.org`,
+								password: "a867h398jdg",
+								verified: ((iIdx % 2) === 0) ? true : false
+							});
+
+						await testUser.register (this.db);
+						this.logger.info (`Registered user: ${testUser.displayName}`);
+					}
+					catch (ex)
+					{
+						this.logger.error (`Error registering user: ${ex}`);
+					}
+				}
 
 				return (true);
 			};
 
-		this.addRoute (new DataRoute (this, async (db: HotDBMySQL) =>
-			{
-				if (this.connection.processor.mode === DeveloperMode.Development)
-				{
-					await db.query (
-						`create table if not exists users (
-							id             INT(10)        NOT NULL AUTO_INCREMENT,
-							name           VARCHAR(256)   DEFAULT '',
-							email          VARCHAR(256)   DEFAULT '',
-							password       VARCHAR(256)   DEFAULT '',
-							verified       INT(1)         DEFAULT '0',
-							registered     DATETIME       DEFAULT NOW(),
-							enabled        INT(1)         DEFAULT '1',
-							PRIMARY KEY (id)
-						)`);
-
-					let results: any = await db.queryOne (`select COUNT(*) from users;`);
-
-					if (results.results["COUNT(*)"] < 1)
-					{
-						let testUsers = [{
-									name: "John",
-									email: "test1@freelight.org",
-									password: "a867h398jdg",
-									verified: true
-								},
-								{
-									name: "Jane",
-									email: "test2@freelight.org",
-									password: "ai97w3a98w3498",
-									verified: true
-								}
-							];
-
-						for (let iIdx = 0; iIdx < testUsers.length; iIdx++)
-						{
-							let testUser = testUsers[iIdx];
-							let verified: number = 0;
-
-							if (testUser.verified === true)
-								verified = 1;
-
-							let result: any = await db.query (
-								`INSERT INTO users (name, email, password, verified) VALUES (?, ?, ?, ?);`, 
-								[testUser.name, testUser.email, testUser.password, verified]);
-
-							if (result.error != null)
-								throw new Error (result.error);
-
-							let id: number = result.results["insertId"];
-						}
-					}
-				}
-			}));
+		this.addRoute (new UserRoute (this, "users"));
+		this.addRoute (new FriendlyAdminRoute (this, "admins"));
 	}
 }
